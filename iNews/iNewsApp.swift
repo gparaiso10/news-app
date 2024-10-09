4d9f046507510e382b4699e642e95ee9bdfe4b51
gparaiso <gparaiso@deloitte.pt>
2024-10-09 11:07:45 +0100

    first commit

---
+++ iNews/iNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNewsApp.swift
+//  iNews
+//
+//  Created by Paraiso, Gustavo Alexandre on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct iNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iNewsTests/iNewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  iNewsTests.swift
+//  iNewsTests
+//
+//  Created by Paraiso, Gustavo Alexandre on 09/10/2024.
+//
+
+import Testing
+@testable import iNews
+
+struct iNewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
